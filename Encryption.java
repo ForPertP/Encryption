9a9a2b95e4ff3de238a14a2274740bba75d0c171
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-05 02:08:37 +0900

    Create Encryption.java

---
+++ Encryption.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'encryption' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String encryption(String s) {
+        // Write your code here
+        s = s.replaceAll("\\s", "");
+
+        int L = s.length();
+
+        int col = (int)Math.ceil(Math.sqrt(L));
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < col; ++i) {
+            for (int j = i; j < L; j += col) {
+                result.append(s.charAt(j));
+            }
+
+            result.append(' ');
+        }
+
+        return result.toString();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.encryption(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
